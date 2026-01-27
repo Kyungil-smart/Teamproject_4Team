a7351132b17186d202ec4030db3d895d9deefc0a
Hooni0_0 <startrack0515@gmail.com>
2026-01-27 22:19:25 +0900

    [Update] 스테이지 ScriptableObject 수정
    박승훈 260127
    몬스터 스폰 관련 데이터,
    웨이브 시간 관련 데이터,
    웨이브 클리어시 획득하는 골드 추가

---
+++ Tower Sewar/Assets/Scripts/박승훈/ScriptableObject/StageData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stage Data", menuName = "Scriptable Object/Stage Data", order = 0)]
+public class StageData : ScriptableObject
+{
+    [SerializeField] List<WaveData> _waveDatas = new List<WaveData>();
+}
+
+[System.Serializable]
+public class WaveData
+{
+    [Header("웨이브 몬스터 데이터")]
+    [SerializeField] private string _monsterName;
+    public string MonsterName => _monsterName;
+    [SerializeField] private int _spawnAmount;
+    public int SpawnAmount => _spawnAmount;
+    [SerializeField] private int _spawnDelay;
+    public int SpawnDelay => _spawnDelay;
+    
+    [Space(15)]
+    [Header("웨이브 시간 데이터")]
+    [SerializeField] private int _waveReadyTime;
+    public int WaveReadyTime => _waveReadyTime;
+    [SerializeField] private int _waveLimitTime;
+    public int WaveLimitTime => _waveLimitTime;
+
+    [Space(15)]
+    [Header("웨이브 데이터")]
+    [SerializeField] private int _clearGold;
+    public int ClearGold => _clearGold;
+    
+}
