6e710d0779024b52f000b9bfd0963a66b56c4aaf
Shrek75 <freshine0@gmail.com>
2026-01-28 15:09:11 +0900

    [Test] 한글주석테스트

---
+++ Tower Sewar/Assets/Scripts/김태욱/testScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // 테스트 한글 주석 입니다
+    // 가나다라마바사아
+    //ㅁ나어루ㅏㅁ너유라ㅓ ㅁ뉴ㅏ어류나멍ㄹ/!!!@#!ㅓㅏㅣㄴㅊㄴ울미만울
+    // asldkjfnbsakljdfbkjasbvkjsa
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
