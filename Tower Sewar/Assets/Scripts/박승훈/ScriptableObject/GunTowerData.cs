d1d9b1be42b3b2806017c78cb1c8454787d5ab06
Hooni0_0 <startrack0515@gmail.com>
2026-01-28 11:35:46 +0900

    [Update] 타워정보 분리
    박승훈 260128
    티어별 타워 정보가 한 오브젝트에 통합되어 따로 관리 하기 위해 분리

--- Tower Sewar/Assets/Scripts/박승훈/ScriptableObject/GunTowerData.cs
+++ Tower Sewar/Assets/Scripts/박승훈/ScriptableObject/GunTowerData.cs
@@ -9,36 +9,16 @@ public class GunTowerData : ScriptableObject
     [SerializeField] private string _towerName;
     public string TowerName => _towerName;
     [SerializeField] private int _towerBuildCost;
-    int TowerBuildCost => _towerBuildCost;
+    public int TowerBuildCost => _towerBuildCost;
     
     [Space(20)]
-    [Header("1단계 타워 정보")]
-    [SerializeField] private int _towerUpCost1;
-    public int TowerUpCost1 => _towerUpCost1;
-    [SerializeField] private float _towerAtt1;
-    public float TowerAtt1 => _towerAtt1;
-    [SerializeField] private float _towerAttSpd1;
-    public float TowerAttSpd1 => _towerAttSpd1;
-    [SerializeField] private float _towerRange1;
-    public float TowerRange1 => _towerRange1;
-    
-    [Space(20)]
-    [Header("2단계 타워 정보")]
-    [SerializeField] private int _towerUpCost2;
-    public int TowerUpCost2 => _towerUpCost2;
-    [SerializeField] private float _towerAtt2;
-    public float TowerAtt2 => _towerAtt2;
-    [SerializeField] private float _towerAttSpd2;
-    public float TowerAttSpd2 => _towerAttSpd2;
-    [SerializeField] private float _towerRange2;
-    public float TowerRange2 => _towerRange2;
-    
-    [Space(20)]
-    [Header("3단계 타워 정보")]
-    [SerializeField] private float _towerAtt3;
-    public float TowerAtt3 => _towerAtt3;
-    [SerializeField] private float _towerAttSpd3;
-    public float TowerAttSpd3 => _towerAttSpd3;
-    [SerializeField] private float _towerRange3;
-    public float TowerRange3 => _towerRange3;
+    [Header("타워 스탯")]
+    [SerializeField] private int _towerUpCost;
+    public int TowerUpCost => _towerUpCost;
+    [SerializeField] private float _towerAtt;
+    public float TowerAtt => _towerAtt;
+    [SerializeField] private float _towerAttDelay;
+    public float TowerAttDelay => _towerAttDelay;
+    [SerializeField] private float _towerRange;
+    public float TowerRange => _towerRange;
 }
