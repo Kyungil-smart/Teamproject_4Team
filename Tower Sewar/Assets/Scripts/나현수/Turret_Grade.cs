8ca48044a6916afb63dd18073c08b60afeb6da1d
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-28 11:28:29 +0900

    [Create] Turret Grade 추가

---
+++ Tower Sewar/Assets/Scripts/나현수/Turret_Grade.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Turret_Grade : MonoBehaviour
+{
+    [SerializeField] private GunTowerData _data = new GunTowerData();
+    private int _curGrade = 1;
+    private int _maxGrade = 3;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void UpgradeTurret()
+    {
+
+    }
+}
