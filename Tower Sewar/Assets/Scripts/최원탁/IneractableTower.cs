f990944d4543696d0da8ba8a7c0c68b5e92ca832
akfvhdl321 <akfvhdl321@gmail.com>
2026-01-29 17:17:15 +0900

    [Update] 레이아웃 및 설치 상호작용  추가 건
    레이캐스트 기반 목표 상호작용 시
    
    레이아웃 표시, F키 상호작용 설치모드 진입
    
    ESC 키 설치모드 취소

---
+++ Tower Sewar/Assets/Scripts/최원탁/IneractableTower.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class InteractableTower : MonoBehaviour, IPlayerInteractable
+{
+    public void OnPlayerInteract()
+    {
+        Debug.Log("타워 상호작용됨");
+
+        // 마우스 락 해제
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
