315f810aeb60245fd2843e4ec7ea9a2766f5d27e
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-01-29 17:03:48 +0900

    [Test] 실행테스트

---
+++ Tower Sewar/Assets/Scripts/나현수/Rocket/Rocket.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Benjathemaker;
+using UnityEngine;
+
+
+public class Rocket : MonoBehaviour
+{
+    RocketRotator rotator;
+    
+}
+`
