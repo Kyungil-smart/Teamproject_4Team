9509c43e9ccfb6f681703a3c4535cb37a748761b
akfvhdl321 <akfvhdl321@gmail.com>
2026-01-29 15:33:34 +0900

    [Update] 아웃라인 기능 건
    아웃라인 기능 진행 중

---
+++ Tower Sewar/Assets/Scripts/최원탁/IPlayerInteractable.cs
@@ -0,0 +1,5 @@
+﻿public interface IPlayerInteractable
+{
+    // 레이캐스트 상호작용
+    void OnPlayerInteract();
+}
---
+++ Tower Sewar/Assets/Scripts/최원탁/IneractableCube.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class InteractableCube : MonoBehaviour, IPlayerInteractable
+{
+    public void OnPlayerInteract()
+    {
+        Debug.Log("큐브 상호작용됨");
+
+        // 테스트용: 마우스 락 해제
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
