cfe6c983eccc4d9d94b902c877b74271e373ef4b
Hooni0_0 <startrack0515@gmail.com>
2026-01-30 14:19:58 +0900

    [Fix] 메뉴 선택 마우스
    박승훈 260130
    메뉴선택 화면에서 클릭 시 커서가 락온 되는 현상 해결

--- Tower Sewar/Assets/Scripts/최원탁/UIcontroller.cs
+++ Tower Sewar/Assets/Scripts/최원탁/UIcontroller.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class UIcontroller : MonoBehaviour
 {
-    [SerializeField] private GameObject TowerSelectionPanel;
+    [SerializeField] private List<GameObject> TowerSelectionPanel = new List<GameObject>();
+    private int _selectedTower = 0;
+    
 
     public void OpenTowerSelection()
     {
-        TowerSelectionPanel.SetActive(true);
+        foreach (GameObject g in TowerSelectionPanel)
+        {
+            g.SetActive(true);
+        }
     }
     
     public void CloseTowerSelection()
     {
-        TowerSelectionPanel.SetActive(false);
+        foreach (GameObject g in TowerSelectionPanel)
+        {
+            g.SetActive(false);
+        }
     }
+
+    public void GunTower()
+    {
+        _selectedTower = (int)TowerType.GunTower;
+        Debug.Log($"{(TowerType)_selectedTower} Selected Tower");
+    }
+    public void CannonTower()
+    {
+        _selectedTower = (int)TowerType.CannonTower;
+        Debug.Log($"{(TowerType)_selectedTower} Selected Tower");
+        
+    }
+    public void IceTower()
+    {
+        _selectedTower = (int)TowerType.IceTower;
+        Debug.Log($"{(TowerType)_selectedTower} Selected Tower");
+        
+    }
+
+    public void BuildSelect()
+    {
+        Debug.Log($"{(TowerType)_selectedTower} Selected BuildTower");
+        CloseTowerSelection();
+    }
+
+    public void TowerUpgrade()
+    {
+        Debug.Log("TowerUpgrade");
+        CloseTowerSelection();
+    }
+}
+
+public enum TowerType
+{
+    GunTower,
+    CannonTower,
+    IceTower
 }
