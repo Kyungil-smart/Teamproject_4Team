c04e0afda0cae6d33087910183fbb52d86d8dd8e
akfvhdl321 <akfvhdl321@gmail.com>
2026-01-30 14:17:02 +0900

    [Update] 구조물 및 맵 통과 건
    구조물을 통과하지 못하는 기능 추가
    
    맵 경계 바깥으로 나가지 못하는 기능 추가
    
    맵 관련 스크립트 추가

---
+++ Tower Sewar/Assets/Scripts/최원탁/MapBoundaryVisualizer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class MapBoundaryVisualizer : MonoBehaviour
+{
+    [SerializeField] private Color gizmoColor = new Color(0f, 1f, 0f, 0.25f);
+
+    private BoxCollider box; // 맵이라서 박스 콜라이더 갖고옴
+
+    private void OnDrawGizmos()
+    {
+        if (box == null)
+            box = GetComponent<BoxCollider>();
+
+        if (box == null)
+            return;
+
+        Gizmos.color = gizmoColor;
+
+        // 월드 좌표 기준
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawCube(box.center, box.size);
+    }
+}
